0d835a6d2f814ca2433d4ab0d5cbf7142843600f
tgni <nitonggui@aliyun.com>
2020-01-18 20:00:18 +0800

    ex3 predict.m

--- machine-learning-ex3/ex3/predict.m
+++ machine-learning-ex3/ex3/predict.m
@@ -4,11 +4,11 @@ function p = predict(Theta1, Theta2, X)
 %   trained weights of a neural network (Theta1, Theta2)
 
 % Useful values
-m = size(X, 1);
-num_labels = size(Theta2, 1);
+m = size(X, 1);		%5000
+num_labels = size(Theta2, 1);	%10
 
 % You need to return the following variables correctly 
-p = zeros(size(X, 1), 1);
+p = zeros(size(X, 1), 1);	%5000x1
 
 % ====================== YOUR CODE HERE ======================
 % Instructions: Complete the following code to make predictions using
@@ -21,15 +21,14 @@ p = zeros(size(X, 1), 1);
 %       can use max(A, [], 2) to obtain the max for each row.
 %
 
-
-
-
-
-
-
-
+X  = [ones(m, 1) X];	% 5000x401
+z2 = Theta1 * X';	% 25x5000
+a2 = sigmoid(z2);	% 25x5000
+a2 = [ones(m, 1), a2']; % 5000x26
+z3 = Theta2 * a2';	% 10x5000
+a3 = sigmoid(z3);	% 10x5000
+[mp, p] = max(a3', [], 2);
 
 % =========================================================================
-
 
 end
